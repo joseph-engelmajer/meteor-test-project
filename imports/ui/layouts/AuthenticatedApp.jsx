c59c532fbcc7a006de91d863cd1ebc0e7b453b6b
Joseph Engelmajer <joseph.engelmajer@gmail.com>
2017-02-01 16:33:40 +0800

    fixes auth

--- imports/ui/layouts/AuthenticatedApp.jsx
+++ imports/ui/layouts/AuthenticatedApp.jsx
@@ -5,30 +5,8 @@ import { Router, Route, browserHistory } from 'react-router';
 
 export default class AuthenticatedApp extends Component {
 
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      isAuthenticated: '',
-    };
-  }
-
-  checkAuthStatus() {
-    if (Meteor.user()) {
-      this.setState({ isAuthenticated: true })
-    } else {
-      this.setState({ isAuthenticated: false })
-    }
-  }
-
   componentWillMount() {
-    if (!this.state.isAuthenticated) {
-      browserHistory.push('/signIn');
-    }
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-    if (!this.state.isAuthenticated) {
+     if (!Meteor.user()) {
       browserHistory.push('/signIn');
     }
   }
